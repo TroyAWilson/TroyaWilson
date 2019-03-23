e671128acc4975aa144f8e003ff4731904d21349
TroyAWilson <troyaustynwilson@gmail.com>
2019-03-23 11:06:00 -0700

    Email form finally works!
    finally got the PHP for this page to work

--- scripts/formSend.php
+++ scripts/formSend.php
@@ -1,28 +1,60 @@
 <?php
-if(!isset($POST['submit']))
+if(!isset($_POST['submit']))
 {
-  echo "error, you need to submit the form!";
+	//This page should not be accessed directly. Need to submit the form.
+	echo "error; you need to submit the form!";
 }
-$name =$_POST['name'];
+$name = $_POST['name'];
 $visitor_email = $_POST['email'];
 $message = $_POST['message'];
 
-// Validation
+//Validate first
 if(empty($name)||empty($visitor_email))
 {
-  echo "Name and email are mandatory!";
-  exit;
+    echo "Name and email are mandatory!";
+    exit;
 }
 
-$email_from = 'troyaustynwilson@gmail.com';
-$email_subject ="New Form Submission";
-$email_body ="You have recieved a message from the user $name \n".
-        "email address: $visitor_email \n".
-        "Here is the message: $message \n".
+if(IsInjected($visitor_email))
+{
+    echo "Bad email value!";
+    exit;
+}
 
-$to ="troyaustynwilson@gmail.com";
-$headers ="From: $email_from \r\n";
+$email_from = 'troyaustynwilson@gmail.com';//<== update the email address
+$email_subject = "New Form submission";
+$email_body = "You have received a new message from the user $name.\n".
+    "Here is the message:\n $message".
 
-// Sends email
+$to = "troyaustynwilson@gmail.com";//<== update the email address
+$headers = "From: $email_from \r\n";
+$headers .= "Reply-To: $visitor_email \r\n";
+//Send the email!
 mail($to,$email_subject,$email_body,$headers);
+//done. redirect to thank-you page.
+
+
+// Function to validate against any email injection attempts
+function IsInjected($str)
+{
+  $injections = array('(\n+)',
+              '(\r+)',
+              '(\t+)',
+              '(%0A+)',
+              '(%0D+)',
+              '(%08+)',
+              '(%09+)'
+              );
+  $inject = join('|', $injections);
+  $inject = "/$inject/i";
+  if(preg_match($inject,$str))
+    {
+    return true;
+  }
+  else
+    {
+    return false;
+  }
+}
+
 ?>
